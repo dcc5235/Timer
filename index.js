34218a024aeab1ba9b2712d2b00c7c3e5f8d79cd
Dany <dany@Danys-MacBook-Pro.local>
2020-11-06 20:35:12 -0600

    timer class added with event listener on start btn

---
+++ index.js
@@ -0,0 +1,21 @@
+class Timer {
+  // Constructor function called automatically when we create new instance of Timer
+  // Pass in references to DOM elements which Timer class will listen to
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log('time to start');
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
