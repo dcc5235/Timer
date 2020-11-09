4b6762a4ad6762077bbab6f4cf4b9114c0015397
Dany <dany@Danys-MacBook-Pro.local>
2020-11-09 09:54:02 -0600

    add timer js file

---
+++ timer.js
@@ -0,0 +1,54 @@
+class Timer {
+  // Constructor function called automatically when we create new instance of Timer
+  // Pass in references to DOM elements which Timer class will listen to
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  // ParseInt removes decimal
+  // ParseFloat supports decimals inside timer
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  };
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
